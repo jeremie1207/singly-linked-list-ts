b3228d2d474dcd32e2160d525267da3b111b10c2
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-03-05 09:42:59 -0500

    implements push

--- MySinglyLinkedList.ts
+++ MySinglyLinkedList.ts
@@ -8,4 +8,26 @@ class MySinglyLinkedList {
         this.tail = null;
         this.size = 0;
     }
+
+    push(value : number) {
+        const newNode = new MyNode(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail!.next = newNode;
+            this.tail = newNode; 
+        }
+        this.size++;
+    }
 }
+
+const list = new MySinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+
+console.log(list);
