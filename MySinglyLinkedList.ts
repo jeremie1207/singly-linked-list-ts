2ab539f92e8e605973468671b19c25a08f58dfad
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-03-05 09:29:06 -0500

    Create MySinglyLinkedList

---
+++ MySinglyLinkedList.ts
@@ -0,0 +1,11 @@
+class MySinglyLinkedList {
+    head: MyNode | null;
+    tail : MyNode | null;
+    size: number;
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+}
